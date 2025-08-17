fa0319351aab485dc40c6dea0fcfe48b9827c046
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-08-17 16:05:25 +0000

    add resources

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('#menu-btn');
+  const closeBtn = document.querySelector('#close-btn');
+  const ul = document.querySelector('#ul');
+
+  // initial state
+  closeBtn.style.visibility = 'hidden';
+  ul.style.visibility = 'hidden';
+
+  function menuButton() {
+    menuBtn.style.visibility = 'hidden';
+    ul.style.visibility = 'visible';
+    closeBtn.style.visibility = 'visible';
+  }
+
+  function closeButton() {
+    closeBtn.style.visibility = 'hidden';
+    ul.style.visibility = 'hidden';
+    menuBtn.style.visibility = 'visible';
+  }
+
+  menuBtn.addEventListener('click', menuButton);
+  closeBtn.addEventListener('click', closeButton);
+});
